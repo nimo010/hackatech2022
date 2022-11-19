3f7041db88b857650cb6dbf7b69091d6e2bbbabb
rafaric <rafaelstrongoli@gmail.com>
2022-11-19 12:52:55 -0300

    app y main

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import main from './paginas/main';
 import './App.css';
 
@@ -7,35 +6,6 @@ function App() {
     <div className="App">
       <main />
     </div>
-=======
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import RegistroPage from "./pages/RegistroPage/RegistroPage";
-import CarreraPage from "./pages/CarreraPage/CarreraPage";
-import MisReservasPage from "./pages/MisReservasPage/MisReservasPage";
-import CrearReviewPage from "../src/pages/CrearReviewPage/CrearReviewPage";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/login" component={LoginPage}></Route>
-        <Route exact path="/registro" component={RegistroPage}></Route>
-
-        <Route exact path="/home" component={HomePage}></Route>
-        <Route
-          exact
-          path="/carreras/:id_carrera/"
-          component={CarreraPage}
-        ></Route>
-        <Route exact path="/crear-review" component={CrearReviewPage}></Route>
-
-        <Route exact path="/not-found" component={NotFoundPage} />
-      </Switch>
-    </BrowserRouter>
->>>>>>> 8513f277da3d2d86d1333606d14cc11032807370
   );
 }
 
